ce944a3933a0a98bea4684066516de57526afc98
cassidyb <cassidybollin32@gmail.com>
2016-10-07 19:09:31 -0700

    commenting out img

--- PokeModule.cs
+++ PokeModule.cs
@@ -2,10 +2,12 @@ using Nancy;
 using System;
 using System.Collections.Generic;
 using ApiCaller;
+
 namespace poke1
 {
+    
     public class PokeModule : NancyModule
-    {
+    {  
         public PokeModule()
         {
             Get("/", async args =>
@@ -14,7 +16,8 @@ namespace poke1
                 long Weight = 0;
                 long Height = 0;
                 object Front = "";
-                // string Sprite = "";
+                // string Sprite = "";                
+                
                 // Our anonymous function is a parameter of type Action that returns a Dictionary
                 await WebRequest.SendRequest("http://pokeapi.co/api/v2/pokemon/25", new Action<Dictionary<string, object>>( JsonResponse =>
                     {
@@ -22,7 +25,7 @@ namespace poke1
                         Weight = (long)JsonResponse["weight"];
                         Height = (long)JsonResponse["height"];
                         Front = (object)JsonResponse["sprites"];
-                        // Sprite = (string)JsonResponse["back_female"];
+                        // Sprite = (string)JsonResponse["front_female"];
                         Console.WriteLine(Front);
                         // Console.WriteLine(Sprite);
                     }
@@ -31,6 +34,7 @@ namespace poke1
                 ViewBag.Weight = Weight;
                 ViewBag.Height = Height;
                 ViewBag.Front = Front;
+                Console.WriteLine("b");
                 // ViewBag.Sprite = Sprite;
                 return View["poke.sshtml"];
             });
