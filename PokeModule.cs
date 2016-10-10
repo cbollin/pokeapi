05cbb1f293eeefe9a19bd9837d0cb0201337eecf
cassidyb <cassidybollin32@gmail.com>
2016-10-10 10:48:59 -0700

    final commit

--- PokeModule.cs
+++ PokeModule.cs
@@ -5,39 +5,44 @@ using ApiCaller;
 
 namespace poke1
 {
-    
     public class PokeModule : NancyModule
-    {  
+    {
         public PokeModule()
         {
-            Get("/", async args =>
+            Get("/{id}", async args =>
             {
                 string Name = "";
+                // object sprites = "";
                 long Weight = 0;
                 long Height = 0;
-                object Front = "";
-                // string Sprite = "";                
-                
+
+                string url = "http://pokeapi.co/api/v2/pokemon/";
+                try
+                {
+                    url += (int)args.id;
+                }
+                catch
+                {
+                    url += "151";
+                }
+
                 // Our anonymous function is a parameter of type Action that returns a Dictionary
-                await WebRequest.SendRequest("http://pokeapi.co/api/v2/pokemon/25", new Action<Dictionary<string, object>>( JsonResponse =>
+                await WebRequest.SendRequest(url, new Action<Dictionary<string, object>>( JsonResponse =>
                     {
                         Name = (string)JsonResponse["name"];
+                        // sprites = (object)JsonResponse["sprites"];
                         Weight = (long)JsonResponse["weight"];
                         Height = (long)JsonResponse["height"];
-                        Front = (object)JsonResponse["sprites"];
-                        // Sprite = (string)JsonResponse["front_female"];
-                        Console.WriteLine(Front);
-                        // Console.WriteLine(Sprite);
+                        // exp = (long)JsonResponse["base_experience"];
+ 
+                        @ViewBag.name = Name;
+                        @ViewBag.weight = Weight;
+                        @ViewBag.height = Height;
+                        // @ViewBag.exp = exp; 
                     }
                 ));
-                ViewBag.Name = Name;
-                ViewBag.Weight = Weight;
-                ViewBag.Height = Height;
-                ViewBag.Front = Front;
-                Console.WriteLine("b");
-                // ViewBag.Sprite = Sprite;
                 return View["poke.sshtml"];
-            });
+            }); 
         }
     }
 }
